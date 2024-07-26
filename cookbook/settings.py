9ed0f1ad0a0ba6b480ebcb57f1e2c826de235566
Neeru Rathour <Neerurathour@gmail.com>
2024-07-26 13:55:35 +0000

    code ready to deploy

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j+2eea@mggt3rin=7af@pv+avwpwg-wsx2)w^3mr&4m=dmftx#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-neerurathou-djangomycoo-pt1ikxsdxgu.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-neerurathou-djangomycoo-pt1ikxsdxgu.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 
 # Application definition
